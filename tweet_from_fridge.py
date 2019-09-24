0c9d99e8561c2f0788696c6158f1f36c1937aa30
Ramsy <55553838+Ramsy65@users.noreply.github.com>
2019-09-24 16:34:04 -0500

    Update tweet_from_fridge.py

--- tweet_from_fridge.py
+++ tweet_from_fridge.py
@@ -2,20 +2,20 @@
 import sys, os, time
 import tweepy
 keys = dict(
-consumer_key='_YOUR_CONSUMER_KEY',
-consumer_secret='_YOUR_SECRET_KEY',
-access_token='_YOUR_ACCESS_TOKEN', 
-access_token_secret='_YOUR_ACCESS_TOKEN_SECRET'
+consumer_key='iJgCUSrDk4nWqPYpWnbQ4cP8i ',
+consumer_secret='NWtdb8AHLV389LyjFBnfES2XtPk551V1TQeU8Gx8r5oEvmpRHE',
+access_token='885544101835309056-gYY2Ob8YOOWslR8vQoB3AluhjernUSz', 
+access_token_secret='nKLPMXAdjTxCWiucuWGsHpOeAyzwaexk0QwZGcOn0Plux'
 )
 
-user = "@YOUR_USERNAME"
+user = "@Ramsy176"
 
-auth = tweepy.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
-auth.set_access_token(keys['access_token'], keys['access_token_secret'])
+auth = tweepy.OAuthHandler(keys['iJgCUSrDk4nWqPYpWnbQ4cP8i'], keys['NWtdb8AHLV389LyjFBnfES2XtPk551V1TQeU8Gx8r5oEvmpRHE'])
+auth.set_access_token(keys['885544101835309056-gYY2Ob8YOOWslR8vQoB3AluhjernUSz'], keys['nKLPMXAdjTxCWiucuWGsHpOeAyzwaexk0QwZGcOn0Plux'])
 api = tweepy.API(auth)
 
 def tweet():
-	message=input("tweet: ")
+	message=input("lost my phone looks like i gotta tweet off of this🤷: ")
 	api.update_status(message)
 	time.sleep(1000)
 if __name__ == "__main__":
